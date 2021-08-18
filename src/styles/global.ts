8b06e58e2a0010f70aea7d9c173a078a29da4473
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-18 16:45:16 -0300

    refatorando global style para pasta

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle, DefaultTheme } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #__next {
+  height: 100vh;
+  color: ${({ theme }) => theme.colors.text};
+  display: flex;
+}
+
+:root {
+  font-size: ${({ theme }) => theme.fontSize};
+}
+`;
+
+export const theme: DefaultTheme = {
+  colors: {
+    detach: "#A29BFE",
+    text: "#636E72",
+    white: "#FFFFFF",
+    black: "#000000",
+    backgroundContainerMusic: "#DFE6E9",
+    playMusic: "#55efc4",
+    playDisable: "#00b894",
+    borderColor: "#C4C4C4",
+  },
+  breakpoints: {
+    small: "780",
+    medium: "1024",
+    large: "1444",
+  },
+  fontSize: "16px",
+};
