ecf8903d904b3fd890779716a38ae255035eb4f2
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-18 14:11:45 -0300

    adicionando styled components ao projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
+
+const theme = {
+  colors : {},
+  breakpoints: {},
+  fontSizes: {}
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles/>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 export default MyApp
