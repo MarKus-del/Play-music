f1b8a75234f91632083e92937cd3101fdc542dc4
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-18 22:01:33 -0300

    criando prototipo da pagina de index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,318 @@
-import type { NextPage } from 'next';
+import { NextPage } from "next";
+import Image from "next/image";
+import { FaHeart, FaPlay, FaSearch } from "react-icons/fa";
+import {
+  Search,
+  Container,
+  Header,
+  ListMusic,
+  MusicItem,
+} from "../styles/index";
+
+type LoaderProps = {
+  src: string;
+  width: string;
+};
+
+const myLoader = ({ src }: LoaderProps) => {
+  return src;
+};
 
 const Home: NextPage = () => {
   return (
-    <div>
-      as
-    </div>
+    <Container>
+      <Search>
+        <input type="text" placeholder="Pesquise sua música aqui" />
+        <button>
+          <FaSearch />
+        </button>
+      </Search>
+
+      <Header>
+        <h1>TOP músicas</h1>
+      </Header>
+
+      <ListMusic>
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+
+        <MusicItem>
+          <Image
+            className="img"
+            loader={myLoader}
+            width={90}
+            height={90}
+            src="https://api.deezer.com/album/234349272/image"
+          />
+
+          <div className="info">
+            <span>Music name</span>
+            <span>By authors</span>
+            <span>in Track</span>
+
+            <span className="more">ver mais</span>
+          </div>
+
+          <div className="play">
+            <button>
+              <FaHeart />
+            </button>
+
+            <button>
+              <FaPlay />
+            </button>
+          </div>
+        </MusicItem>
+      </ListMusic>
+    </Container>
   );
 };
 
---
+++ src/styles/index.ts
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  width: 75vw;
+  padding: 1rem;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Search = styled.div`
+  height: 100px;
+  width: 30rem;
+  display: flex;
+  align-items: center;
+
+  input {
+    width: 27rem;
+    height: 3rem;
+    padding: 1rem;
+    border-radius: 1rem 0 0 1rem;
+    border: none;
+    border: 2px solid ${({ theme }) => theme.colors.text};
+    outline: none;
+
+
+    &:hover {
+      border: 2px solid black;
+    }
+  }
+
+  button {
+    height: 3rem;
+    width: 5rem;
+    padding: 1rem;
+    border-radius: 0 1rem 1rem 0;
+    border: 2px solid ${({ theme }) => theme.colors.text};
+    cursor: pointer;
+		font-size: 1.5rem;
+		color: ${({ theme }) => theme.colors.detach};
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+    &:hover {
+      border: 2px solid black;
+    }
+  }
+`;
+
+export const Header = styled.header`
+  width: auto;
+  padding: 1rem;
+  border-bottom: 3px solid ${({ theme }) => theme.colors.borderColor};
+  margin-bottom: 1rem;
+  h1 {
+    color: ${({ theme }) => theme.colors.detach};
+    font-size: 3rem;
+  }
+`;
+
+export const ListMusic = styled.ul`
+  flex: 1;
+  padding: 1rem;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+	grid-template-rows: repeat(5, 1fr);
+  grid-gap: 1rem;
+`;
+
+export const MusicItem = styled.li`
+  list-style: none;
+  width: 100%;
+  height: 6rem;
+  background-color: ${({ theme }) => theme.colors.backgroundContainerMusic};
+  border-radius: 1rem;
+  display: grid;
+  grid-template-columns: 100px 2fr 1fr;
+  grid-template-areas: "photo info play";
+
+  .img {
+    grid-area: photo;
+    background-color: red;
+    border-radius: 1rem 0 0 1rem;
+  }
+
+  .info {
+    grid-area: info;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0.5rem;
+
+    .more {
+      cursor: pointer;
+			font-weight: 800;
+			font-size: 1.5rem;
+    }
+  }
+
+  .play {
+    grid-area: play;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+			background: none;
+			border: none;
+      margin-right: 1rem;
+      font-size: 3rem;
+      color: ${({ theme }) => theme.colors.detach};
+			cursor: pointer;
+			
+			&:hover {
+				color: ${({ theme }) => theme.colors.playMusic};
+			}
+    }
+  }
+`;
