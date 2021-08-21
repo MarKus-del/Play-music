609c2eb21f620ac004f37ab52608e35bf59d1443
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-20 21:21:03 -0300

    limpando index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,20 @@
 import { NextPage } from "next";
-import { ChangeEvent, useState } from "react";
 import { AxiosResponse } from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { FaSearch, FaAngleRight, FaAngleLeft } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-import { apiFront, apiNext } from "../services/api";
+import { apiNext } from "../services/api";
 import { storeWrapper } from "../store";
 import { Track } from "../services/types";
 import { load, update, searchRequest } from "../store/module/music/actions";
 import { RootState } from "../store/module/rootReducer";
 import {
-  convertAxiosResponseToStoreState,
   convertResponseTrackToEntityTrack,
 } from "../utils/converts";
 
 import Header from "../components/Header";
 import MusicItem from "../components/MusicItem";
-import {
-  Search,
-  Container,
-  ListMusic,
-  Pagination,
-  PaginationButton,
-} from "../styles/index";
+import { Container, ListMusic } from "../styles/index";
+import InputSearch from "../components/InputSearch";
 
 type ResponseData = {
   data: Track[];
@@ -36,110 +28,13 @@ type RequestSearchDeezer = {
 };
 
 const Home: NextPage<ResponseData> = () => {
-  const [search, setSearch] = useState("");
-  const dispatch = useDispatch();
-  const { tracks, isLoading, currentPage, totalPageList, searchName } =
-    useSelector((state: RootState) => state.music);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
-
-  const handleRequest = async (): Promise<RequestSearchDeezer> => {
-    const { data }: AxiosResponse<RequestSearchDeezer> = await apiFront.get(
-      `/track/search?name="${search}"&index=0`
-    );
-
-    return convertAxiosResponseToStoreState(data);
-  };
-
-  const searchTrack = async () => {
-    dispatch(searchRequest());
-    const { data, total, next, prev } = await handleRequest();
-
-    const totalPagesInSearch = Math.round(total / 10);
-
-    dispatch(
-      update({
-        tracks: data,
-        currentPage: 1,
-        totalPageList: totalPagesInSearch,
-        searchName: search,
-      })
-    );
-  };
-
-  const nextPage = async () => {
-    dispatch(searchRequest());
-
-    const nextPage = currentPage + 1;
-    const index = (nextPage - 1) * 10;
-
-    const { data }: AxiosResponse<RequestSearchDeezer> = await apiFront.get(
-      `/track/search?name="${searchName}"&index=${index}`
-    );
-
-    const { data: tracks } = convertAxiosResponseToStoreState(data);
-
-    dispatch(
-      update({
-        currentPage: nextPage,
-        totalPageList,
-        searchName,
-        tracks,
-      })
-    );
-  };
-
-  const prevPage = async () => {
-    dispatch(searchRequest());
-
-    const prevPage = currentPage - 1;
-    const index = (prevPage - 1) * 10;
-
-    const { data }: AxiosResponse<RequestSearchDeezer> = await apiFront.get(
-      `/track/search?name="${searchName}"&index=${index}`
-    );
-
-    const { data: tracks } = convertAxiosResponseToStoreState(data);
-
-    dispatch(
-      update({
-        currentPage: prevPage,
-        totalPageList,
-        searchName,
-        tracks,
-      })
-    );
-  };
+  const { tracks, isLoading } = useSelector((state: RootState) => state.music);
 
   return (
     <Container>
-      <Search>
-        <input
-          type="text"
-          placeholder="Pesquise sua música aqui"
-          onChange={(e) => handleChange(e)}
-        />
-        <button onClick={searchTrack}>
-          <FaSearch />
-        </button>
-      </Search>
+      <InputSearch />
 
-      <Header title={searchName}>
-        <Pagination>
-          <PaginationButton>
-            <button disabled={currentPage === 1} onClick={prevPage}>
-              <FaAngleLeft />
-            </button>
-            <span className="page-index">{currentPage}</span>
-            <button onClick={nextPage} disabled={currentPage >= totalPageList}>
-              <FaAngleRight />
-            </button>
-          </PaginationButton>
-          <span>Paginas: {totalPageList}</span>
-        </Pagination>
-      </Header>
+      <Header />
 
       <ListMusic>
         {isLoading ? (
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -10,45 +10,6 @@ export const Container = styled.main`
   margin: 0 auto;
 `;
 
-export const Search = styled.div`
-  height: 100px;
-  width: 30rem;
-  display: flex;
-  align-items: center;
-
-  input {
-    width: 27rem;
-    height: 3rem;
-    padding: 1rem;
-    border-radius: 1rem 0 0 1rem;
-    border: none;
-    border: 2px solid ${({ theme }) => theme.colors.text};
-    outline: none;
-
-    &:hover {
-      border: 2px solid black;
-    }
-  }
-
-  button {
-    height: 3rem;
-    width: 5rem;
-    padding: 1rem;
-    border-radius: 0 1rem 1rem 0;
-    border: 2px solid ${({ theme }) => theme.colors.text};
-    cursor: pointer;
-    font-size: 1.5rem;
-    color: ${({ theme }) => theme.colors.detach};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    &:hover {
-      border: 2px solid black;
-    }
-  }
-`;
-
 export const ListMusic = styled.ul`
   flex: 1;
   padding: 1rem;
@@ -57,55 +18,4 @@ export const ListMusic = styled.ul`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: repeat(5, 1fr);
   grid-gap: 1rem;
-`;
-
-export const Pagination = styled.div`
-  width: 10rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  span {
-    margin-top: 0.5rem;
-    font-weight: 800;
-    text-transform: uppercase;
-    font-size: 1rem;
-  }
-`;
-
-export const PaginationButton = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  button {
-    padding: 0.3rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0.5rem;
-    border: none;
-    cursor: pointer;
-    color: ${({ theme }) => theme.colors.text};
-    border-radius: 1rem;
-
-    &:disabled {
-      cursor: auto;
-    }
-
-    &:not(:disabled):hover {
-      font-size: 1rem;
-      background-color: ${({ theme }) => theme.colors.text};
-      color: ${({ theme }) => theme.colors.white};
-    }
-  }
-
-  .page-index {
-    margin: 0.5rem;
-    color: ${({ theme }) => theme.colors.detach};
-    font-weight: 800;
-    font-size: 1.5rem;
-    background: none;
-  }
 `;
