2af17c1c7b4d1708c43291cee7609f3b2f7adc95
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-20 21:20:10 -0300

    refatorando logica de paginação para Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,86 @@
+import { AxiosResponse } from "axios";
 import { ReactNode } from "react";
-import { HeaderContainer } from "./styles";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { apiFront } from "../../services/api";
+import { Track } from "../../services/types";
+import { searchRequest, update } from "../../store/module/music/actions";
+import { RootState } from "../../store/module/rootReducer";
+import { convertAxiosResponseToStoreState } from "../../utils/converts";
+import { HeaderContainer, Pagination, PaginationButton } from "./styles";
 
-type HeaderProps = {
-  title: string;
-  children: ReactNode
-}
+type RequestSearchDeezer = {
+  data: Track[];
+  next?: string | null;
+  total: number;
+  prev?: string | null;
+};
 
-export default function Header({ title, children }: HeaderProps) {
+export default function Header() {
+  const dispatch = useDispatch();
+  const { currentPage, totalPageList, searchName } = useSelector(
+    (state: RootState) => state.music
+  );
+
+  const nextPage = async () => {
+    dispatch(searchRequest());
+
+    const nextPage = currentPage + 1;
+    const index = (nextPage - 1) * 10;
+
+    const { data }: AxiosResponse<RequestSearchDeezer> = await apiFront.get(
+      `/track/search?name="${searchName}"&index=${index}`
+    );
+
+    const { data: tracks } = convertAxiosResponseToStoreState(data);
+
+    dispatch(
+      update({
+        currentPage: nextPage,
+        totalPageList,
+        searchName,
+        tracks,
+      })
+    );
+  };
+
+  const prevPage = async () => {
+    dispatch(searchRequest());
+
+    const prevPage = currentPage - 1;
+    const index = (prevPage - 1) * 10;
+
+    const { data }: AxiosResponse<RequestSearchDeezer> = await apiFront.get(
+      `/track/search?name="${searchName}"&index=${index}`
+    );
+
+    const { data: tracks } = convertAxiosResponseToStoreState(data);
+
+    dispatch(
+      update({
+        currentPage: prevPage,
+        totalPageList,
+        searchName,
+        tracks,
+      })
+    );
+  };
+
   return (
     <HeaderContainer>
-      <h1>{title}</h1>
-      {children}
+      <h1>{searchName}</h1>
+      <Pagination>
+        <PaginationButton>
+          <button disabled={currentPage === 1} onClick={prevPage}>
+            <FaAngleLeft />
+          </button>
+          <span className="page-index">{currentPage}</span>
+          <button onClick={nextPage} disabled={currentPage >= totalPageList}>
+            <FaAngleRight />
+          </button>
+        </PaginationButton>
+        <span>Paginas: {totalPageList}</span>
+      </Pagination>
     </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   width: auto;
@@ -11,5 +11,56 @@ export const HeaderContainer = styled.header`
   h1 {
     color: ${({ theme }) => theme.colors.detach};
     font-size: 3rem;
+  }
+`;
+
+export const Pagination = styled.div`
+  width: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    margin-top: 0.5rem;
+    font-weight: 800;
+    text-transform: uppercase;
+    font-size: 1rem;
+  }
+`;
+
+export const PaginationButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    padding: 0.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0.5rem;
+    border: none;
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.text};
+    border-radius: 1rem;
+
+    &:disabled {
+      cursor: auto;
+    }
+
+    &:not(:disabled):hover {
+      font-size: 1rem;
+      background-color: ${({ theme }) => theme.colors.text};
+      color: ${({ theme }) => theme.colors.white};
+    }
+  }
+
+  .page-index {
+    margin: 0.5rem;
+    color: ${({ theme }) => theme.colors.detach};
+    font-weight: 800;
+    font-size: 1.5rem;
+    background: none;
   }
 `;
