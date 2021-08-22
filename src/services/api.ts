854f71d906a55bbe64be94523c9ee0fefd9872bd
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-22 15:07:03 -0300

    adicionando variavel de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const addressApiFront = process.env.NEXT_PUBLIC_HOSTNAME || 'http://127.0.0.1:3000/api'
+
 export const apiNext = axios.create({
   baseURL: "https://api.deezer.com",
 });
 
 export const apiFront = axios.create({
-  baseURL: "http://192.168.150.86:3000/api",
+  baseURL: addressApiFront,
 });
