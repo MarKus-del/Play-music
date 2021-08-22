7837661da61ac4baa2d319a99efb752e9f479e65
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-22 16:18:56 -0300

    consertando endereco da propria api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const addressApiFront =
-  process.env.NEXT_PUBLIC_HOSTNAME || "http://127.0.0.1:3000/api";
-
 export const apiNext = axios.create({
   baseURL: "https://api.deezer.com",
 });
 
 export const apiFront = axios.create({
-  baseURL: addressApiFront,
+  baseURL: '/api',
 });
